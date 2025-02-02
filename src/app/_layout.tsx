bfc288d998d543f935b4cf09fbc45f7754c14c3a
Ashutosh JHa <ashutoshj.cs.21@nitj.ac.in>
2025-02-02 23:52:46 +0530

    fixing the revenue cat issue

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,20 +1,20 @@
 import { Stack } from 'expo-router';
 import '../../global.css';
 
-import Purchases from 'react-native-purchases';
+// import Purchases from 'react-native-purchases';
 import { useEffect } from 'react';
 import { Platform } from 'react-native';
 
-const apiKey = Platform.select({
-  ios: process.env.EXPO_PUBLIC_REVENUE_CAT_APPLE_KEY,
-  android: process.env.EXPO_PUBLIC_REVENUE_CAT_ANDROID_KEY,
-});
+// const apiKey = Platform.select({
+//   ios: process.env.EXPO_PUBLIC_REVENUE_CAT_APPLE_KEY,
+//   android: process.env.EXPO_PUBLIC_REVENUE_CAT_ANDROID_KEY,
+// });
 
 export default function RootLayout() {
-  useEffect(() => {
-    Purchases.setLogLevel(Purchases.LOG_LEVEL.DEBUG);
-    Purchases.configure({ apiKey });
-  }, []);
+  // useEffect(() => {
+  //   Purchases.setLogLevel(Purchases.LOG_LEVEL.DEBUG);
+  //   Purchases.configure({ apiKey });
+  // }, []);
 
   return (
     <Stack>
